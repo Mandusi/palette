7baee17b0d536d7a55cc04a13a2455b72cb9d1b8
Mandusi <osman_ekinci66@hotmail.com>
2024-01-17 05:57:16 +0300

    responsive canvas

--- clotheditor.js
+++ clotheditor.js
@@ -1,3 +1,90 @@
 export default class ClothEditor {
-    constructor(root, product) {}
+  constructor(root, product) {
+    this.root = root
+    this.product = product
+
+    // MAIN DIV ELEMENT
+    this.mainContainer = document.createElement('div')
+    this.mainContainer.setAttribute('id', 'main-container')
+    this.mainContainerWrapper = document.createElement('div')
+    this.mainContainerWrapper.setAttribute('id', 'main-container-wrapper')
+    this.root.append(this.mainContainerWrapper)
+    this.mainContainerWrapper.append(this.mainContainer)
+
+    // PRODUCT IMAGE ELEMENT
+    this.productImg = document.createElement('img')
+    this.productImg.setAttribute('src', this.product.src)
+    this.productImg.setAttribute('id', 'product-img')
+    this.mainContainer.append(this.productImg)
+
+    // RATIOS
+    this.prodCanvasRatio =
+      this.product.height / (this.product.canvas.y1 - this.product.canvas.y0)
+
+    this.canvasHWRatio =
+      (this.product.canvas.y1 - this.product.canvas.y0) /
+      (this.product.canvas.x1 - this.product.canvas.x0)
+
+    this.positionRatioY = this.product.canvas.y0 / this.product.height
+    this.positionRatioX = this.product.canvas.x0 / this.product.width
+    // CREATE CANVAS ELEMENT
+    this.canvasContainer = document.createElement('div')
+    this.canvasContainer.setAttribute('id', 'canvas-container-wrapper')
+    this.canvasEl = document.createElement('canvas')
+    this.canvasContainer.append(this.canvasEl)
+    this.mainContainer.append(this.canvasContainer)
+
+    this.clientPosition = {
+      x: this.product.canvas.x0 * this.prodCanvasRatio,
+      y: this.product.canvas.y0 * this.prodCanvasRatio,
+    }
+
+    this.canvasHeight = this.productImg.clientHeight / this.prodCanvasRatio
+    this.canvasWidth = this.canvasHeight / this.canvasHWRatio
+
+    console.log(this.productImg.clientWidth, this.productImg.clientHeight)
+    this.productImg.onload = this.createCanvas.bind(this)
+    window.onresize = this.resizeWrapper.bind(this)
+  }
+
+  createCanvas() {
+    this.canvas = new window.fabric.Canvas(this.canvasEl)
+    this.canvas.setDimensions({
+      height: this.canvasHeight,
+      width: this.canvasWidth,
+    })
+
+    this.style = document.createElement('style')
+    document.body.append(this.style)
+    this.style.innerText = `#canvas-container-wrapper {
+        height: ${this.canvasHeight}px;
+        width: ${this.canvasWidth}px;
+        top: ${this.productImg.clientHeight * this.positionRatioY}px;
+        left: ${this.productImg.clientWidth * this.positionRatioX}px;
+      }`
+
+    const text = new window.fabric.Text('text')
+    this.canvas.add(text)
+  }
+  resizeWrapper() {
+    const imgHeight = document.getElementById('product-img').clientHeight
+    this.canvasHeight = imgHeight / this.prodCanvasRatio
+    this.canvasWidth = this.canvasHeight / this.canvasHWRatio
+
+    const scale = this.canvasWidth / this.canvas.getWidth()
+    const zoom = this.canvas.getZoom() * scale
+    this.canvas.setViewportTransform([zoom, 0, 0, zoom, 0, 0])
+    this.canvas.setDimensions({
+      height: this.canvasHeight,
+      width: this.canvasWidth,
+    })
+
+    this.style.innerText = `#canvas-container-wrapper {
+      height: ${this.canvasHeight}px;
+      width: ${this.canvasWidth}px;
+      top: ${this.productImg.clientHeight * this.positionRatioY}px;
+      left: ${this.productImg.clientWidth * this.positionRatio}px;
+  }`
+    console.log(this.productImg.clientWidth, this.productImg.clientHeight)
+  }
 }
--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-import ClothEditor from './clotheditor'
+import ClothEditor from './clotheditor.js'
 
 const root = document.querySelector('#root')
 
 const product = {
-    src: 'https://url.to/the/image.png',
-    width: 1000,
-    height: 1400,
-    canvas: {
-        x0: 160,
-        x1: 700,
-        y0: 200,
-        y1: 700,
-    },
+  src: './assets/icons/shirt-1.png',
+  width: 844,
+  height: 957,
+  canvas: {
+    x0: 210,
+    y0: 250,
+    x1: 620,
+    y1: 865,
+  },
+}
+const product1 = {
+  src: './assets/icons/shirt-2.png',
+  width: 860,
+  height: 1078,
+  canvas: {
+    x0: 223,
+    y0: 151,
+    x1: 630,
+    y1: 868,
+  },
+}
+const product2 = {
+  src: './assets/icons/bag-1.jpg',
+  width: 691,
+  height: 691,
+  canvas: {
+    x0: 225,
+    y0: 400,
+    x1: 495,
+    y1: 590,
+  },
 }
 
 new ClothEditor(root, product)
